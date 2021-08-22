6233d73dbce1a580786a56ef98c681cd71522c3a
Aharon Lubitsch <59685123+ahronlu@users.noreply.github.com>
2021-08-22 11:56:19 +0300

    Create CourseFormScreen.js

---
+++ client/src/screens/CourseFormScreen.js
@@ -0,0 +1,185 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Alert,
+  Row,
+  Col,
+  Card,
+  Form,
+  FormCheck,
+  Spinner,
+} from "react-bootstrap";
+import { updateBootcamp, createBootcamp } from "../actions/bootcampActions";
+import { getUserDetails } from "../actions/userActions";
+
+const initialState = {
+  name: "",
+  description: "",
+  website: "",
+  phone: "",
+  email: "",
+  address: "",
+  location: { formattedAddress: "" },
+  careers: [],
+  jobAssistance: false,
+  jobGuarantee: false,
+  acceptGi: false,
+  housing: false,
+};
+
+const BootcampFormScreen = ({ match, history }) => {
+  const bootcampId = match.params.id;
+
+  const [bootcamp, setBootcamp] = useState(initialState);
+
+  const dispatch = useDispatch();
+
+  const bootcampUpdate = useSelector((state) => state.bootcampUpdate);
+  const {
+    loading: updateLoading,
+    error: updateError,
+    success: updateSuccess,
+  } = bootcampUpdate;
+
+  const bootcampCreate = useSelector((state) => state.bootcampCreate);
+  const {
+    loading: createLoading,
+    error: createError,
+    success: createSuccess,
+  } = bootcampCreate;
+
+  const userDetails = useSelector((state) => state.userDetails);
+  const { loading, user } = userDetails;
+
+  useEffect(() => {
+    dispatch(getUserDetails());
+  }, [match, dispatch]);
+
+  const handleChange = (e) => {
+    e.target.type === "checkbox"
+      ? setBootcamp({
+          ...bootcamp,
+          [e.target.name]: e.target.checked,
+        })
+      : setBootcamp({
+          ...bootcamp,
+          [e.target.name]: e.target.value,
+        });
+  };
+
+  const handleCareersChange = (e) => {
+    const options = e.target;
+    let value = [];
+    for (let i = 0, l = options.length; i < l; i++) {
+      if (options[i].selected) {
+        value.push(options[i].value);
+      }
+    }
+    setBootcamp({ ...bootcamp, careers: value });
+  };
+
+  const SubmitBoocamp = async (e) => {
+    e.preventDefault();
+    try {
+      if (bootcampId) {
+        await dispatch(updateBootcamp(bootcamp));
+      } else {
+        await dispatch(createBootcamp(bootcamp));
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+			<div class="row">
+				<div class="col-md-8 m-auto">
+					<div class="card bg-white py-2 px-4">
+						<div class="card-body">
+							<a
+								href="manage-courses.html"
+								class="btn btn-link text-secondary my-3"
+								><i class="fas fa-chevron-left"></i> Manage Courses</a
+							>
+							<h1 class="mb-2">DevWorks Bootcamp</h1>
+							<h3 class="text-primary mb-4">Add Course</h3>
+							<form action="manage-bootcamp.html">
+								<div class="form-group">
+									<label>Course Title</label>
+									<input
+										type="text"
+										name="title"
+										class="form-control"
+										placeholder="Title"
+									/>
+								</div>
+								<div class="form-group">
+									<label>Duration</label>
+									<input
+										type="number"
+										name="duration"
+										placeholder="Duration"
+										class="form-control"
+									/>
+									<small class="form-text text-muted"
+										>Enter number of weeks course lasts</small
+									>
+								</div>
+								<div class="form-group">
+									<label>Course Tuition</label>
+									<input
+										type="number"
+										name="tuition"
+										placeholder="Tuition"
+										class="form-control"
+									/>
+									<small class="form-text text-muted">USD Currency</small>
+								</div>
+								<div class="form-group">
+									<label>Minimum Skill Required</label>
+									<select name="minimumSkill" class="form-control">
+										<option value="beginner" selected>Beginner (Any)</option>
+										<option value="intermediate">Intermediate</option>
+										<option value="advanced">Advanced</option>
+									</select>
+								</div>
+								<div class="form-group">
+									<textarea
+										name="description"
+										rows="5"
+										class="form-control"
+										placeholder="Course description summary"
+										maxlength="500"
+									></textarea>
+									<small class="form-text text-muted"
+										>No more than 500 characters</small
+									>
+								</div>
+								<div class="form-check">
+									<input
+										class="form-check-input"
+										type="checkbox"
+										name="scholarshipAvailable"
+										id="scholarshipAvailable"
+									/>
+									<label class="form-check-label" for="scholarshipAvailable">
+										Scholarship Available
+									</label>
+								</div>
+								<div class="form-group mt-4">
+									<input
+										type="submit"
+										value="Add Course"
+										class="btn btn-dark btn-block"
+									/>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+  );
+};
+
+export default BootcampFormScreen;
