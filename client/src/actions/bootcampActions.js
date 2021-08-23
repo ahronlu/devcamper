77ba805c04d4f33b527b33e8951725152c58890b
Aharon Lubitsch <59685123+ahronlu@users.noreply.github.com>
2021-08-23 07:37:31 +0300

    Update bootcampActions.js

--- client/src/actions/bootcampActions.js
+++ client/src/actions/bootcampActions.js
@@ -208,3 +208,38 @@ export const createBootcampReview =
       });
     }
   };
+
+export const getMyBootcamp = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: BOOTCAMP_DETAILS_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const {
+      data: { data },
+    } = await axios.delete(`/api/bootcamps/getmybootcamp`, config);
+
+    dispatch({
+      type: BOOTCAMP_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: BOOTCAMP_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
